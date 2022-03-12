17cc327b05664bfdd31d2650aebcd334664b77a5
andreadicastri <andrea.dicastri@edu.cobianchi.it>
2022-03-12 15:50:22 +0100

    Test di modifica

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Cinema
         </a>
       </header>
     </div>
